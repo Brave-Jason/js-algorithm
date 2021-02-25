e9f341f410db5085b4fddbe1b906962cb8659701
朱维维 <zhuweiwei@zhuweiweideMacBook-Pro.local>
2021-02-25 23:51:45 +0800

    前序

---
+++ 栈/144/index.js
@@ -0,0 +1,14 @@
+var preorderTraversal = function(root) {
+    const res = [];
+    const stack = [];
+      while(stack.length > 0 || root){
+        while(root){
+            res.push(root.val);
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        root = root.right;
+    }
+    return res;
+};
